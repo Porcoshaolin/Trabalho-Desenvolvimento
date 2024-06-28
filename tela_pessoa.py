c28d3a26940668f9ffb55d1eb5867bc0d280108a
Porcoshaolin <164963243+Porcoshaolin@users.noreply.github.com>
2024-06-28 18:58:36 -0300

    Update tela_pessoa.py

--- tela_pessoa.py
+++ tela_pessoa.py
@@ -1,23 +1,58 @@
+import PySimpleGUI as sg
 from tela import Tela
 
 
 class TelaPessoa(Tela):
+    self.__window = None
+    self.init_opcoes()
 
     def tela_opcoes(self):
-        print("\n")
-        print("-------- PESSOAS ----------")
-        print("Escolha uma das opções abaixo:")
-        print("1 - Listar pessoas")
-        print("2 - Adicionar pessoa")
-        print("3 - Alterar pessoa")
-        print("4 - Excluir pessoa")
-        print("5 - Retornar")
-        print("\n")
+        self.init_opcoes()
+        button, values = self.open()
+        if button == '1':
+            opcao = 1
+        elif button == '2':
+            opcao = 2
+        elif button == '3':
+            opcao = 3
+        elif button == '4':
+            opcao = 4
+        elif button == '5':
+            opcao = 5
+        else:
+            opcao = None
 
-        opcao = self.ler_num_inteiro("Opção: ", [1,2,3,4,5])
-        return opcao
+    def init_opcoes(self, nome_pessoa=''):
 
-    def pegar_pessoa(self):
+        sg.ChangeLookAndFeel('DarkGray16')
+        layout = [
+            [sg.Text('Informações de Pessoas', font=('Century Gothic', 25))],
+            [sg.Text("Escolha uma das opções abaixo:", font=("Open Sans", 15), size=(30, 1))],
+            [sg.Button("Listar pessoas", font=("Open Sans", 15), size=(30, 1), key='1')],
+            [sg.Button("Adicionar pessoa", font=("Open Sans", 15), size=(30, 1), key='2')],
+            [sg.Button("Alterar pessoa", font=("Open Sans", 15), size=(30, 1), key='3')],
+            [sg.Button("Excluir pessoa", font=("Open Sans", 15), size=(30, 1), key='4')],
+            [sg.Button("Retornar", font=("Open Sans", 15), size=(30, 1), key='5')
+        ]
+
+        self.__window = sg.Window('Opções em informação de pessoas', element_justification='c').Layout(layout)
+
+def pegar_pessoa(self, nome_pessoa=''):
+
+        sg.ChangeLookAndFeel('DarkGray16')
+        layout = [
+            [sg.Text('Insira o nome da pessoa', font=("Open Sans", 12))],
+            [sg.InputText(nome_pessoa, key='pessoa')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+
+        ]
+
+        self.__window = sg.Window('Opções em informação de pessoas', element_justification='c').Layout(layout)
+
+        button, values = self.open()
+
+
+def pegar_pessoa(self):
         nome = input("Nome: ")
         print("\n")
         if isinstance(nome, str) and nome is not None:
@@ -31,14 +66,3 @@ class TelaPessoa(Tela):
         print("\n")
         if isinstance(nome, str) and nome is not None:
             return nome
-
-
-
-
-
-
-
-
-
-
-
